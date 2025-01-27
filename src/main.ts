53857b650e983ca54930053d00a3100881296e98
camilahsp <116742754+camilahsp@users.noreply.github.com>
2025-01-27 10:21:14 -0400

    Arrumando erros.

--- src/main.ts
+++ src/main.ts
@@ -7,49 +7,6 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-class BancoMysql {
-  private connection: mysql.Connection;
-
-  async criarConexao() {
-    this.connection = await mysql.createConnection({
-      host: process.env.dbhost || "localhost",
-      user: process.env.dbuser || "root",
-      password: process.env.dbpassword || "",
-      database: process.env.dbname || "banco1022a",
-      port: process.env.dbport ? parseInt(process.env.dbport) : 3306
-    });
-  }
-
-  async finalizarConexao() {
-    await this.connection.end();
-  }
-
-  async listar() {
-    const [result] = await this.connection.query("SELECT * from produtos");
-    return result;
-  }
-
-  async listarPorId(id: number) {
-    const [result] = await this.connection.query("SELECT * from produtos WHERE id = ?", [id]);
-    return result;
-  }
-
-  async inserir(produto: any) {
-    const [result] = await this.connection.query("INSERT INTO produtos SET ?", produto);
-    return result;
-  }
-
-  async excluir(id: number) {
-    const [result] = await this.connection.query("DELETE from produtos WHERE id = ?", [id]);
-    return result;
-  }
-
-  async alterar(id: number, produto: any) {
-    const [result] = await this.connection.query("UPDATE produtos SET ? WHERE id = ?", [produto, id]);
-    return result;
-  }
-}
-
 app.get("/produtos", async (req, res) => {
   try {
     const banco = new BancoMysql()
@@ -67,7 +24,7 @@ app.get("/produtos/:id", async (req, res) => {
   try {
     const banco = new BancoMysql()
     await banco.criarConexao()
-    const result = await banco.listarPorId(parseInt(req.params.id))
+    const result = await banco.listarPorId((req.params.id))
     await banco.finalizarConexao()
     res.send(result)
   } catch (e) {
@@ -95,7 +52,7 @@ app.delete("/produtos/:id", async (req, res) => {
   try {
     const banco = new BancoMysql()
     await banco.criarConexao()
-    const result = await banco.excluir(parseInt(req.params.id))
+    const result = await banco.excluir((req.params.id))
     await banco.finalizarConexao()
     res.status(200).send("Produto excluido com sucesso id: " + req.params.id)
   } catch (e) {
@@ -109,7 +66,7 @@ app.put("/produtos/:id", async (req, res) => {
   const produto = { nome, descricao, preco, imagem }
   const banco = new BancoMysql()
   await banco.criarConexao()
-  const result = await banco.alterar(parseInt(req.params.id), produto)
+  const result = await banco.alterar((req.params.id), produto)
   await banco.finalizarConexao()
   res.status(200).send("Produto alterado com sucesso id: " + req.params.id)
 })
